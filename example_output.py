fa7ae6aaaf819ea07bf65add8ae9add6f5072ca5
Fabian Woltermann <fwoltermann@gmail.com>
2018-02-23 10:13:09 +0100

    Add project files.

---
+++ example_output.py
@@ -0,0 +1,9 @@
+from BackendTask import PropertySet
+from BackendTask import Property
+
+property_set = PropertySet('Test Property Set 1')
+property_set.properties.append(Property('Width', 100))
+property_set.properties.append(Property('Height', 100))
+property_set.properties.append(Property('ThermalTransmittance', 0.9))
+property_set.properties.append(Property('FireResistance', 'Class 4'))
+property_set.properties.append(Property('WindLoadRating', 'Class 3'))
